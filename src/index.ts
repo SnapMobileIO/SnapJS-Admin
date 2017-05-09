ca19939291f563b1fb0498fa3738d083db05ae17
Brandon Passley <bpassley@gmail.com>
2017-05-09 16:27:40 -0500

    Removed unused files

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SampleComponent } from './sample.component';
-import { SampleDirective } from './sample.directive';
-import { SamplePipe } from './sample.pipe';
-import { SampleService } from './sample.service';
-
 import { SnapJSAdminModule } from './admin/admin.module';
 
-export * from './sample.component';
-export * from './sample.directive';
-export * from './sample.pipe';
-export * from './sample.service';
 export * from './admin/admin.module';
 
 @NgModule({
@@ -19,21 +10,15 @@ export * from './admin/admin.module';
     SnapJSAdminModule
   ],
   declarations: [
-    SampleComponent,
-    SampleDirective,
-    SamplePipe
   ],
   exports: [
-    SampleComponent,
-    SampleDirective,
-    SamplePipe
   ]
 })
-export class SampleModule {
+export class AdminModule {
   static forRoot(): ModuleWithProviders {
     return {
-      ngModule: SampleModule,
-      providers: [SampleService]
+      ngModule: AdminModule,
+      providers: []
     };
   }
 }
