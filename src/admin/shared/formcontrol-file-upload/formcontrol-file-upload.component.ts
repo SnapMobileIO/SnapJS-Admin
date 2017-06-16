84367d554ed814209771765410eab4b28de8e77d
Catie Stallings <catiestallings@Caties-MacBook-Pro.local>
2017-06-16 15:35:20 -0500

    Add file and file array types to form

--- src/admin/shared/formcontrol-file-upload/formcontrol-file-upload.component.ts
+++ src/admin/shared/formcontrol-file-upload/formcontrol-file-upload.component.ts
@@ -85,6 +85,7 @@ export class FormControlFileUploadComponent implements OnInit {
   }
 
   addItem(item: any = {}) {
+    debugger
     const control = <FormArray>this.form.controls[this.field];
     control.push(this.initItem(item));
   }
