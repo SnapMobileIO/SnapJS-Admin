83dc31df904181f51fe6aecb388c5e3293a3949c
Catie Stallings <catiestallings@Caties-MacBook-Pro.local>
2017-06-30 09:18:57 -0500

    WIP - dropdown for filter field is working

--- src/admin/shared/filter/filter.component.ts
+++ src/admin/shared/filter/filter.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { FormControl, FormArray, FormGroup, FormBuilder } from '@angular/forms';
+import { AdminService } from '../../admin.service';
 
 @Component({
   selector: 'app-filter',
@@ -13,6 +15,21 @@ export class FilterComponent {
   @Input() itemsPerPage: number;
   @Input() skip: number;
   @Input() sort: string;
+  form: FormGroup = new FormGroup({});
 
-  constructor() {}
+  constructor(
+    public adminService: AdminService
+  ) {}
+
+  addFilters() {
+    console.log('** add filters')
+  }
+
+  submit() {
+    console.log('*** submitting form')
+  }
+
+  clearFilters() {
+    console.log('**** clearing filters')
+  }
 }
