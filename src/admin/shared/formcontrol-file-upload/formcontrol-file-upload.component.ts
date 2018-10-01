c82b1d03b730ba73f738d68e3b41f1e28babf42e
kadinvanvalin <kadin@Kadins-MBP.attlocal.net>
2018-10-01 14:20:30 -0500

    add snapjs-admin as dependancy

--- src/admin/shared/formcontrol-file-upload/formcontrol-file-upload.component.ts
+++ src/admin/shared/formcontrol-file-upload/formcontrol-file-upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ChangeDetectorRef }   from '@angular/core';
+import { Component, Input, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { FileUploader } from 'ng2-file-upload';
 import { File } from './file';
